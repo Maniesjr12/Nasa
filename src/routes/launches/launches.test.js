c6f5f318c280d1d3205950a09f30894e10a70a57
Muhammed <sulaimonmuhammed707@gail.com>
2023-01-17 14:31:02 +0100

    added pm2 for cluster management

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -11,19 +11,25 @@ describe("Test GET /launches", () => {
 });
 
 describe("Test POST /launches", () => {
-  test("It should respond with 201 success", async () => {
-    const objectWithDate = {
-      mission: "Newone123",
-      rocket: "ZTMISS6",
-      target: "KPS142",
-      launchDate: "january, 12 2023",
-    };
-    const objectWitoutDate = {
-      mission: "Newone123",
-      rocket: "ZTMISS6",
-      target: "KPS142",
-    };
+  const objectWitoutDate = {
+    mission: "Newone123",
+    rocket: "ZTMISS6",
+    target: "KPS142",
+  };
+  const objectWithInvalidDate = {
+    mission: "Newone123",
+    rocket: "ZTMISS6",
+    target: "KPS142",
+    launchDate: "januamand34ry, 12 2023",
+  };
 
+  const objectWithDate = {
+    mission: "Newone123",
+    rocket: "ZTMISS6",
+    target: "KPS142",
+    launchDate: "january, 12 2023",
+  };
+  test("It should respond with 201 success", async () => {
     const response = await request(app)
       .post("/launches")
       .send(objectWithDate)
@@ -34,5 +40,28 @@ describe("Test POST /launches", () => {
     const responseDate = new Date(response.body.launchDate).valueOf();
     expect(requestDate).toBe(responseDate);
     expect(response.body).toMatchObject(objectWitoutDate);
+  });
+  test("It should catch missing required", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(objectWitoutDate)
+      .expect(400)
+      .expect("Content-Type", /json/);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+  });
+
+  test("it should catch invalid dates", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(objectWithInvalidDate)
+      .expect(400)
+      .expect("Content-Type", /json/);
+
+    expect(response.body).toStrictEqual({
+      error: "invalid date property property",
+    });
   });
 });
