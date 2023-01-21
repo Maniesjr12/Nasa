07a2f37c36ff7651c89c4fb2453f448a8283770b
Muhammed <sulaimonmuhammed707@gail.com>
2023-01-21 17:37:36 +0100

    fixed launch test

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -1,67 +1,79 @@
 const request = require("supertest");
 const app = require("../../app");
+const {
+  mongooseConnect,
+  mongooseDisconnect,
+} = require("../../services/mongodb");
 
-describe("Test GET /launches", () => {
-  test("It should respond with 200 success", async () => {
-    const response = await request(app)
-      .get("/launches")
-      .expect(200)
-      .expect("Content-Type", /json/);
+describe("Testing launch API", () => {
+  beforeAll(async () => {
+    await mongooseConnect();
   });
-});
-
-describe("Test POST /launches", () => {
-  const objectWitoutDate = {
-    mission: "Newone123",
-    rocket: "ZTMISS6",
-    target: "KPS142",
-  };
-  const objectWithInvalidDate = {
-    mission: "Newone123",
-    rocket: "ZTMISS6",
-    target: "KPS142",
-    launchDate: "januamand34ry, 12 2023",
-  };
-
-  const objectWithDate = {
-    mission: "Newone123",
-    rocket: "ZTMISS6",
-    target: "KPS142",
-    launchDate: "january, 12 2023",
-  };
-  test("It should respond with 201 success", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(objectWithDate)
-      .expect(201)
-      .expect("Content-Type", /json/);
-
-    const requestDate = new Date(objectWithDate.launchDate).valueOf();
-    const responseDate = new Date(response.body.launchDate).valueOf();
-    expect(requestDate).toBe(responseDate);
-    expect(response.body).toMatchObject(objectWitoutDate);
+  afterAll(async () => {
+    await mongooseDisconnect();
   });
-  test("It should catch missing required", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(objectWitoutDate)
-      .expect(400)
-      .expect("Content-Type", /json/);
-
-    expect(response.body).toStrictEqual({
-      error: "Missing required launch property",
+  describe("Test GET /launches", () => {
+    test("It should respond with 200 success", async () => {
+      const response = await request(app)
+        .get("/launches")
+        .expect(200)
+        .expect("Content-Type", /json/);
     });
   });
 
-  test("it should catch invalid dates", async () => {
-    const response = await request(app)
-      .post("/launches")
-      .send(objectWithInvalidDate)
-      .expect(400)
-      .expect("Content-Type", /json/);
+  describe("Test POST /launches", () => {
+    const objectWitoutDate = {
+      mission: "Newone123",
+      rocket: "ZTMISS6",
+      target: "Kepler-1410 b",
+    };
+    const objectWithInvalidDate = {
+      mission: "Newone123",
+      rocket: "ZTMISS6",
+      target: "Kepler-1410 b",
+      launchDate: "januamand34ry, 12 2023",
+    };
 
-    expect(response.body).toStrictEqual({
-      error: "invalid date property property",
+    const objectWithDate = {
+      mission: "Newone123",
+      rocket: "ZTMISS6",
+      target: "Kepler-1410 b",
+      launchDate: "january, 12 2023",
+    };
+    test("It should respond with 201 success", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(objectWithDate)
+        .expect(201)
+        .expect("Content-Type", /json/);
+
+      const requestDate = new Date(objectWithDate.launchDate).valueOf();
+      const responseDate = new Date(response.body.launchDate).valueOf();
+      expect(requestDate).toBe(responseDate);
+      expect(response.body).toMatchObject(objectWitoutDate);
+    });
+    test("It should catch missing required", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(objectWitoutDate)
+        .expect(400)
+        .expect("Content-Type", /json/);
+
+      expect(response.body).toStrictEqual({
+        error: "Missing required launch property",
+      });
+    });
+
+    test("it should catch invalid dates", async () => {
+      const response = await request(app)
+        .post("/launches")
+        .send(objectWithInvalidDate)
+        .expect(400)
+        .expect("Content-Type", /json/);
+
+      expect(response.body).toStrictEqual({
+        error: "invalid date property property",
+      });
     });
   });
 });
