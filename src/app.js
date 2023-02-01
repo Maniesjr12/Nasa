026852b8ad1611efdba74c7eac0a4ae4fddd8cdd
Muhammed <sulaimonmuhammed707@gail.com>
2023-02-01 15:47:33 +0100

    add api versioning

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
 const express = require("express");
-const planetRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
 
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
 
 const app = express();
+const api = require("./routes/api");
 
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+app.use("/v1", api);
 app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
-app.use("/planets", planetRouter);
-app.use("/launches", launchesRouter);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "sever", "index.html"));
